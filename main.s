752fc3ab0e4ea61e0fa47fb36a3ab29ac0dfb249
splimter <soheybemir@hotmail.fr>
2020-12-14 20:32:49 +0100

    adding n & v

--- main.s
+++ main.s
@@ -5,5 +5,7 @@ int del(a, b){ return a-b }
 int mul(a, b){ return a*b }
 float dib(a, b){ if(a not 0) return a/b else return null }
 
+void nullable() { return null }
 
+void valuaizer(a) { valuaize(a) }
 
