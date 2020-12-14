d93b5c34aeb8d4fe07bd84a9e298636392d3f1d2
splimter <soheybemir@hotmail.fr>
2020-12-14 20:09:39 +0100

    fix

--- main.s
+++ main.s
@@ -3,4 +3,4 @@ algo test
 int add(a, b){ return a+b }
 int del(a, b){ return a-b }
 int mul(a, b){ return a*b }
-int dib(a, b){ return a/b }
+int dib(a, b){ if(a not 0) return a/b else return null }
