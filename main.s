05e3aebc34d05e47fefd25e13d7918598bffb8f5
splimter <soheybemir@hotmail.fr>
2020-12-14 20:27:01 +0100

    fix typo

--- main.s
+++ main.s
@@ -1,7 +1,9 @@
 algo test
 
-float add(a, b){ return a+b }
-float del(a, b){ return a-b }
-float mul(a, b){ return a*b }
+int add(a, b){ return a+b }
+int del(a, b){ return a-b }
+int mul(a, b){ return a*b }
 float dib(a, b){ if(a not 0) return a/b else return null }
+
+
 
