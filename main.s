862713e4817928e9b49a8d7c3bc08e775b5cc901
splimter <soheybemir@hotmail.fr>
2020-12-14 20:10:26 +0100

    fix types

--- main.s
+++ main.s
@@ -1,6 +1,7 @@
 algo test
 
-int add(a, b){ return a+b }
-int del(a, b){ return a-b }
-int mul(a, b){ return a*b }
-int dib(a, b){ if(a not 0) return a/b else return null }
+float add(a, b){ return a+b }
+float del(a, b){ return a-b }
+float mul(a, b){ return a*b }
+float dib(a, b){ if(a not 0) return a/b else return null }
+
